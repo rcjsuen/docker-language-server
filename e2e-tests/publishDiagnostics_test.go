d1e29fa7b3dcdbb86853e2e410e327c6cebf6230
Remy Suen <remy.suen@docker.com>
2025-04-09 11:49:19 -0400

    Disable telemetry in the tests
    Signed-off-by: Remy Suen <remy.suen@docker.com>

--- e2e-tests/publishDiagnostics_test.go
+++ e2e-tests/publishDiagnostics_test.go
@@ -59,6 +59,7 @@ func TestPublishDiagnostics(t *testing.T) {
 }
 
 func initialize(t *testing.T, conn *jsonrpc2.Conn, initializeParams protocol.InitializeParams) {
+	initializeParams.InitializationOptions = map[string]string{"telemetry": "off"}
 	var initializeResult *protocol.InitializeResult
 	err := conn.Call(context.Background(), protocol.MethodInitialize, initializeParams, &initializeResult)
 	require.NoError(t, err)
