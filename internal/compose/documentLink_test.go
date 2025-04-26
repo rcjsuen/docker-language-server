f5587778601549dea685722bddfc245615183667
Remy Suen <remy.suen@docker.com>
2025-04-26 18:47:31 -0400

    Allow opening a referenced image's page in Compose files
    When a service in a Compose file references an image directly, we should
    be able to open the page on Docker Hub or GHCR or wherever in an
    external browser.
    
    Signed-off-by: Remy Suen <remy.suen@docker.com>

--- internal/compose/documentLink_test.go
+++ internal/compose/documentLink_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestDocumentLink(t *testing.T) {
+func TestDocumentLink_IncludedFiles(t *testing.T) {
 	testsFolder := filepath.Join(os.TempDir(), "composeDocumentLinkTests")
 	composeFilePath := filepath.Join(testsFolder, "docker-compose.yml")
 	referencedFilePath := filepath.Join(testsFolder, "file.yml")
@@ -43,6 +43,195 @@ func TestDocumentLink(t *testing.T) {
 		},
 	}
 
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			doc := document.NewComposeDocument("docker-compose.yml", 1, []byte(tc.content))
+			links, err := DocumentLink(context.Background(), composeStringURI, doc)
+			require.NoError(t, err)
+			require.Equal(t, tc.links, links)
+		})
+	}
+}
+
+func TestDocumentLink_ImageLinks(t *testing.T) {
+	testCases := []struct {
+		name    string
+		content string
+		links   []protocol.DocumentLink
+	}{
+		{
+			name: "image: alpine",
+			content: `
+services:
+  test:
+    image: alpine`,
+			links: []protocol.DocumentLink{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 3, Character: 11},
+						End:   protocol.Position{Line: 3, Character: 17},
+					},
+					Target:  types.CreateStringPointer("https://hub.docker.com/_/alpine"),
+					Tooltip: types.CreateStringPointer("https://hub.docker.com/_/alpine"),
+				},
+			},
+		},
+		{
+			name: "image: alpine:1.23",
+			content: `
+services:
+  test:
+    image: alpine:1.23`,
+			links: []protocol.DocumentLink{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 3, Character: 11},
+						End:   protocol.Position{Line: 3, Character: 17},
+					},
+					Target:  types.CreateStringPointer("https://hub.docker.com/_/alpine"),
+					Tooltip: types.CreateStringPointer("https://hub.docker.com/_/alpine"),
+				},
+			},
+		},
+		{
+			name: "image: grafana/grafana",
+			content: `
+services:
+  test:
+    image: grafana/grafana`,
+			links: []protocol.DocumentLink{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 3, Character: 11},
+						End:   protocol.Position{Line: 3, Character: 26},
+					},
+					Target:  types.CreateStringPointer("https://hub.docker.com/r/grafana/grafana"),
+					Tooltip: types.CreateStringPointer("https://hub.docker.com/r/grafana/grafana"),
+				},
+			},
+		},
+		{
+			name: "image: grafana/grafana:1.23",
+			content: `
+services:
+  test:
+    image: grafana/grafana:1.23`,
+			links: []protocol.DocumentLink{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 3, Character: 11},
+						End:   protocol.Position{Line: 3, Character: 26},
+					},
+					Target:  types.CreateStringPointer("https://hub.docker.com/r/grafana/grafana"),
+					Tooltip: types.CreateStringPointer("https://hub.docker.com/r/grafana/grafana"),
+				},
+			},
+		},
+		{
+			name: "image: ghcr.io/super-linter/super-linter",
+			content: `
+services:
+  test:
+    image: ghcr.io/super-linter/super-linter`,
+			links: []protocol.DocumentLink{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 3, Character: 11},
+						End:   protocol.Position{Line: 3, Character: 44},
+					},
+					Target:  types.CreateStringPointer("https://ghcr.io/super-linter/super-linter"),
+					Tooltip: types.CreateStringPointer("https://ghcr.io/super-linter/super-linter"),
+				},
+			},
+		},
+		{
+			name: "image: ghcr.io/super-linter/super-linter:1.23",
+			content: `
+services:
+  test:
+    image: ghcr.io/super-linter/super-linter:1.23`,
+			links: []protocol.DocumentLink{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 3, Character: 11},
+						End:   protocol.Position{Line: 3, Character: 44},
+					},
+					Target:  types.CreateStringPointer("https://ghcr.io/super-linter/super-linter"),
+					Tooltip: types.CreateStringPointer("https://ghcr.io/super-linter/super-linter"),
+				},
+			},
+		},
+		{
+			name: "image: mcr.microsoft.com/powershell",
+			content: `
+services:
+  test:
+    image: mcr.microsoft.com/powershell`,
+			links: []protocol.DocumentLink{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 3, Character: 11},
+						End:   protocol.Position{Line: 3, Character: 39},
+					},
+					Target:  types.CreateStringPointer("https://mcr.microsoft.com/artifact/mar/powershell"),
+					Tooltip: types.CreateStringPointer("https://mcr.microsoft.com/artifact/mar/powershell"),
+				},
+			},
+		},
+		{
+			name: "image: mcr.microsoft.com/powershell:1.23",
+			content: `
+services:
+  test:
+    image: mcr.microsoft.com/powershell:1.23`,
+			links: []protocol.DocumentLink{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 3, Character: 11},
+						End:   protocol.Position{Line: 3, Character: 39},
+					},
+					Target:  types.CreateStringPointer("https://mcr.microsoft.com/artifact/mar/powershell"),
+					Tooltip: types.CreateStringPointer("https://mcr.microsoft.com/artifact/mar/powershell"),
+				},
+			},
+		},
+		{
+			name: "image: mcr.microsoft.com/windows/servercore",
+			content: `
+services:
+  test:
+    image: mcr.microsoft.com/windows/servercore`,
+			links: []protocol.DocumentLink{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 3, Character: 11},
+						End:   protocol.Position{Line: 3, Character: 47},
+					},
+					Target:  types.CreateStringPointer("https://mcr.microsoft.com/artifact/mar/windows/servercore"),
+					Tooltip: types.CreateStringPointer("https://mcr.microsoft.com/artifact/mar/windows/servercore"),
+				},
+			},
+		},
+		{
+			name: "image: mcr.microsoft.com/windows/servercore:1.23",
+			content: `
+services:
+  test:
+    image: mcr.microsoft.com/windows/servercore:1.23`,
+			links: []protocol.DocumentLink{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 3, Character: 11},
+						End:   protocol.Position{Line: 3, Character: 47},
+					},
+					Target:  types.CreateStringPointer("https://mcr.microsoft.com/artifact/mar/windows/servercore"),
+					Tooltip: types.CreateStringPointer("https://mcr.microsoft.com/artifact/mar/windows/servercore"),
+				},
+			},
+		},
+	}
+
+	composeStringURI := fmt.Sprintf("file:///%v", strings.TrimPrefix(filepath.ToSlash(filepath.Join(os.TempDir(), "compose.yaml")), "/"))
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			doc := document.NewComposeDocument("docker-compose.yml", 1, []byte(tc.content))
