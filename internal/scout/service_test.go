450316387d87ec0abfea42fdce1405aff63a48ab
Remy Suen <remy.suen@docker.com>
2025-07-29 15:13:25 -0400

    Update CVE results for the Scout tests
    Signed-off-by: Remy Suen <remy.suen@docker.com>

--- internal/scout/service_test.go
+++ internal/scout/service_test.go
@@ -390,7 +390,7 @@ func TestGetHovers_IgnoresSpecifics(t *testing.T) {
 			result: &protocol.Hover{
 				Contents: protocol.MarkupContent{
 					Kind:  protocol.MarkupKindMarkdown,
-					Value: "Image vulnerabilities:   0C   0H   3M   6L \r\n\r\nRecommended tags:\n\n<table>\n<tr><td><code>25.04</code></td><td align=\"right\">  0C</td><td align=\"right\">  0H</td><td align=\"right\">  3M</td><td align=\"right\">  5L</td><td align=\"right\"></td></tr>\n<tr><td><code>25.10</code></td><td align=\"right\">  0C</td><td align=\"right\">  0H</td><td align=\"right\">  0M</td><td align=\"right\">  0L</td><td align=\"right\"></td></tr>\n</table>\n",
+					Value: "Image vulnerabilities:   0C   0H   4M   6L \r\n\r\nRecommended tags:\n\n<table>\n<tr><td><code>25.04</code></td><td align=\"right\">  0C</td><td align=\"right\">  0H</td><td align=\"right\">  4M</td><td align=\"right\">  5L</td><td align=\"right\"></td></tr>\n<tr><td><code>25.10</code></td><td align=\"right\">  0C</td><td align=\"right\">  0H</td><td align=\"right\">  0M</td><td align=\"right\">  0L</td><td align=\"right\"></td></tr>\n</table>\n",
 				},
 			},
 		},
@@ -406,7 +406,7 @@ func TestGetHovers_IgnoresSpecifics(t *testing.T) {
 			result: &protocol.Hover{
 				Contents: protocol.MarkupContent{
 					Kind:  protocol.MarkupKindMarkdown,
-					Value: "Recommended tags:\n\n<table>\n<tr><td><code>25.04</code></td><td align=\"right\">  0C</td><td align=\"right\">  0H</td><td align=\"right\">  3M</td><td align=\"right\">  5L</td><td align=\"right\"></td></tr>\n<tr><td><code>25.10</code></td><td align=\"right\">  0C</td><td align=\"right\">  0H</td><td align=\"right\">  0M</td><td align=\"right\">  0L</td><td align=\"right\"></td></tr>\n</table>\n",
+					Value: "Recommended tags:\n\n<table>\n<tr><td><code>25.04</code></td><td align=\"right\">  0C</td><td align=\"right\">  0H</td><td align=\"right\">  4M</td><td align=\"right\">  5L</td><td align=\"right\"></td></tr>\n<tr><td><code>25.10</code></td><td align=\"right\">  0C</td><td align=\"right\">  0H</td><td align=\"right\">  0M</td><td align=\"right\">  0L</td><td align=\"right\"></td></tr>\n</table>\n",
 				},
 			},
 		},
@@ -422,7 +422,7 @@ func TestGetHovers_IgnoresSpecifics(t *testing.T) {
 			result: &protocol.Hover{
 				Contents: protocol.MarkupContent{
 					Kind:  protocol.MarkupKindMarkdown,
-					Value: "Image vulnerabilities:   0C   0H   3M   6L ",
+					Value: "Image vulnerabilities:   0C   0H   4M   6L ",
 				},
 			},
 		},
